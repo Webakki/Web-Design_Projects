5995ac8f640fefb17dbce26d7d5a5e9d1ea63173
Aakash Sathwara <115859475+Webakki@users.noreply.github.com>
2023-09-26 22:35:48 -0700

    Add files via upload

---
+++ Chrome_extention/index.js
@@ -0,0 +1,16 @@
+const getJokes = async () => {
+    try{
+        const res = await fetch("https://api.chucknorris.io/jokes/random");
+        const data = await res.json();
+        const myjoke = document.querySelector("#myjoke");
+        myjoke.innerHTML = data.value;
+
+
+    }       catch (error) {}
+  
+};
+
+
+window.addEventListener("load", () => {
+    getJokes();
+});
